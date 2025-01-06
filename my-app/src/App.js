fdc182aa011cc2039118405725669e6c58ee0e11
tzeeeeee@gmail.com <tzeeeeee@gmail.com>
2025-01-06 17:50:26 +0900

    react_practice

---
+++ my-app/src/App.js
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import "./App.css";
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from "./components/ExpenseList";
+
+class App extends Component {
+  render (){
+    return (
+      <main className="main-container">
+        <h1>예산 계산기</h1> 
+        <div style={{width:'100%',backgroundColor: 'red', padding: '1rem'}}>
+          {/* ExpenseForm */}
+          <ExpenseForm />
+        </div>
+        <div style={{width: '100%', backgroundColor:'blue', padding: '1rem'}}>
+          {/* ExpenseList */}
+          <ExpenseList />
+        </div>
+        <div style={{display: 'flex', justifyContent: 'end', marginTop:'1rem'}}>
+          <p style={{fontSize: '2rem'}}>
+            총 지출:
+            <span>원</span>
+          </p>
+          
+        </div>
+          </main>
+    )
+  }
+}
+
+export default App;
